d70909ffb988b40485fe7f61a34875cdfbb49415
DRAKJESUS <91555283+DRAKJESUS@users.noreply.github.com>
2021-10-14 18:03:45 -0500

    Update LataChocolate.m

--- LataChocolate.m
+++ LataChocolate.m
@@ -1,2 +1,16 @@
-%Aqui iremos subiendo todos los codigos
-disp 'pongan algo para que deje subirlo'
+%Octave Script
+%School:       Tecnologico de Estudios Superiores de Jilotepec
+%Title:        Lata para envasar chocolate
+%Descripcion:  Lo que haremos sera resolver un problema en el que tendremos que encontrar las medidas
+%que minimicen los costos de la lata, es decir, el area minima que se debe emplear para cada bote
+%Problema:     La lata para envasar chocolate:
+
+%              Una compañía  usa  latas de forma cilíndrica para envasar  chocolate  en  polvo  en su
+%              presentación de 400 gramos. Encuentra las dimensiones que minimicen los costos de
+%              la lata (área mínima de la hojalata que se debe emplear en cada bote).
+
+%Author:       Jorge Miranda Zuñiga
+%Team:         1. Jorge Miranda Zuñiga
+%              2. Angel Jesus Santiago Hernández
+%              3. Angel Manuel Zarco Valerio
+%Date:         14/10/2021
